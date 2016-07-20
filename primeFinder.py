4b29a03e5a9b9bc78909aa8d464175ace33b0fa3
ScouserInTrousers <ScouserInTrousers@users.noreply.github.com>
2016-07-19 23:09:15 -0600

    Added prime counting function as well as prime factorization function; began the antiprime function

--- primeFinder.py
+++ primeFinder.py
@@ -37,3 +37,35 @@ def nearestPrime(X):
     else:
         print 'Whoa, this is neat! There are two primes that are nearest to %d: %d and %d! Nobody knows if this happens infinitely often: see http://math.stackexchange.com/a/82668 for more details!' % (X, first_prime_before_x, first_prime_after_x)
         return
+
+def decomposeInteger(X, FToA = False):
+    """Returns the tuple of prime factors of the given integer. Credit for this implementation must go to the author: http://stackoverflow.com/a/412942/4747798"""
+    assert type(X) == int
+    if isPrime(X): return (1, X)
+    factors = []
+    d=2
+    while X > 1:
+        while X % d == 0:
+            factors.append(d)
+            X /= d
+        d = d + 1
+        if d*d > X:
+            if X > 1: factors.append(X)
+            break
+    if not FToA: return tuple(factors)
+    else: return {f:factors.count(f) for f in set(factors)}
+    #sanity check: reduce(lambda x,y: x*y, map(pow, a.keys(),a.values()))i == X
+
+
+def pi(X):
+    """This is an implementation of the Prime Counting Function, i.e. the amount of primes not exceeding X."""
+    assert type(X) == int
+    if isPrime(X): return 1
+    return len(decomposeInteger(X,True).keys())
+
+def antiPrime(n):
+    """This function returns the nth antiprime, i.e. the smallest composite integer with n prime factors. """
+What I want to be able to make is a function that returns tuples of the prime
+#factors and the powers on the primes, so that I can move forward with all the
+#fun theorems. Also, want to implement an antiprimes function i.e. the Numberphile
+#post about the nth most composite number 
