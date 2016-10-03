0593899a2ce0cf64458e8e739e724c4b9133ef2d
ScouserInTrousers <ScouserInTrousers@users.noreply.github.com>
2016-10-02 22:36:10 -0600

    Refactor modulo.py and add fn to primeFinder.py

--- primeFinder.py
+++ primeFinder.py
@@ -101,5 +101,34 @@ def pi(z):
     return len( [x for x in range(1,z+1) if isPrime(x)])
     #Possible speed up: return sum(map(isPrime, range(1,z+2)))
 
+def text2int(textnum, numwords={}):
+        if not numwords:
+                  units = [
+                                  "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+                                          "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+                                                  "sixteen", "seventeen", "eighteen", "nineteen",
+                                                        ]
+
+                        tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+    scales = ["hundred", "thousand", "million", "billion", "trillion"]
+
+    numwords["and"] = (1, 0)
+    for idx, word in enumerate(units):    numwords[word] = (1, idx)
+    for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
+    for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
+
+    current = result = 0
+    for word in textnum.split():
+        if word not in numwords:
+            raise Exception("Illegal word: " + word)
+
+        scale, increment = numwords[word]
+        current = current * scale + increment
+        if scale > 100:
+            result += current
+            current = 0
+
+    return result + current
 #Create a function that makes a call to OEIS and returns the URLs of the sequences in which#a given integer appears. Also, want to implement an antiprimes function i.e. the
 #Numberphile post about the nth most composite number 
