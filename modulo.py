8572771c39fb80782bc1390739f5439165585f0e
ScouserInTrousers <ScouserInTrousers@users.noreply.github.com>
2016-10-02 22:36:10 -0600

    Refactor modulo.py and add fn to primeFinder.py
    Change name of primeFinder.py
    
    Fix formatting errors.
    
    Fixed indentation

--- modulo.py
+++ modulo.py
@@ -1,27 +1,22 @@
 def modulo(dividend, divisor):
-	"""This function imitates the modulo operator, %, for learning purposes.
+    """This function imitates the modulo operator, %, for learning purposes.
 	
-	:arguments: 
-		dividend: integer to be divided
-		divisor: integer by which the dividend will be divided
-	:returns: 
-		remainder of the division problem  dividend / divisor as long as divisor is non-zero 
-	"""
+    :arguments: 
+            dividend: integer to be divided
+            divisor: integer by which the dividend will be divided
+    :returns: 
+            remainder of the division problem  dividend / divisor as long as divisor is non-zero 
+    """
 
     if divisor == 0: 
-    	print 'Cannot divide by 0!'
+        print 'Cannot divide by 0!'
     	return
 
     elif not (isinstance(dividend,int) & isinstance(divisor,int)): 
         print 'Dividend AND divisor must be integers.'
         return 
 
-    result = 0
-    if divisor == dividend: return result 
     while dividend >= divisor: 
         dividend -= divisor
 
-    result = dividend
-    return result 
-	
-	
+    return dividend 
